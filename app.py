b619516878cf1cc45969788569e633438f9a453e
Bill Pezzullo <bpezzullo@gmail.com>
2020-08-09 17:34:51 -0400

    added comments and finished app

--- app.py
+++ app.py
@@ -40,14 +40,17 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+@app.route("/api/v1.0/")
 @app.route("/")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"localhost:5000/api/v1.0/precipitation<br/>"
-        f"localhost:5000/api/v1.0/stations<br/>"
-        f"localhost:5000/api/v1.0/tobs"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/Start date<br/>"
+        f"/api/v1.0/Start date/end date"
     )
 
 
@@ -59,16 +62,17 @@ def precipitation():
     """Return a precipitation for the last 12 months"""
     # Design a query to retrieve the last 12 months of precipitation data and plot the results
 
-    # Calculate the date 1 year ago from the last data point in the database
+    # Calculate the date 1 year from the last data point in the database
     data = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
     last_date = datetime.strptime(data[0], '%Y-%m-%d') - dt.timedelta(days=365)
 
+    # pull the data from that point (last_date)
     data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > last_date).all()
 
     session.close()
 
-        # Create a dictionary from the row data and append to a list of all_passengers
+    # Create a dictionary from the row data and append to a dictionary of all the results
     results_list = []
     for date, prcp in data:
         Precipitation_dict = {}
@@ -86,6 +90,7 @@ def stations():
 
     """Return a list of stations and activity"""
 
+    # fine the stations and provide the results of how active they are.
     active = session.query(Measurement.station, func.count(Measurement.station))\
             .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
 
@@ -101,24 +106,69 @@ def tobs():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """return date on the most active station"""
+    """return temperature observations"""
 
+    # Find the most active station
     active = session.query(Measurement.station)\
             .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()
 
+    # find the last date in the database
     date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
+    # subtract an year (365 days)
     last_date = datetime.strptime(date[0], '%Y-%m-%d') - dt.timedelta(days=365)
 
+    # use the results to query the database for the min, max, and average
     sel = [Measurement.station, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs), func.count(Measurement.tobs)]
     tobs_results = session.query(*sel).filter(Measurement.station == active[0]).filter(Measurement.date > last_date).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
+    # Convert list of tuples into tobs list
     tobs = list(np.ravel(tobs_results))
 
     return jsonify(tobs)
+
+@app.route("/api/v1.0/<start>", defaults={"end": None})
+@app.route("/api/v1.0/<start>/<end>")
+def vacation(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """return temperature information on the range provided"""
+    # check the start date.  Is is a date and in the right format.
+    try:
+        valid_start_date = datetime.strptime(start, "%Y-%m-%d")
+    except:
+        # if not then the exception will be thrown.  Close the session and return
+        session.close()
+        return f"Invalid start date.  Please provide a date in the format YYYY-MM-DD"
+    # do the same thing for the end date, but first check to see if there is an end date provided
+    try:
+        # IF end date is None then nothing was provided so use the last date of the database
+        if end is None:
+            last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+            end = last_date[0]
+
+        # Verify the correct format
+        valid_end_date = datetime.strptime(end, "%Y-%m-%d")
+
+    except:
+        # if not then the exception will be thrown.  Close the session and return
+        session.close()
+        return f"Invalid end date.  Please provide a date in the format YYYY-MM-DD or leave blank"
+
+    # use the dates to pul the min, max and average dates for the timeperiod
+    sel = [func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs), func.count(Measurement.tobs)]
+    vac_results = session.query(*sel).filter(Measurement.date >= valid_start_date)\
+        .filter(Measurement.date <= valid_end_date).all()
+
+    session.close()
+
+    # Convert list of tuples into vac list
+    vac = list(np.ravel(vac_results))
+
+    return jsonify(vac)
 
 if __name__ == '__main__':
     app.run(debug=True)
